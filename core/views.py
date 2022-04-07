4cafd33261dbd5460e4c53ce43693d22b6083c3e
Felipe Braghittoni <felipe.braghittoni1999@gmail.com>
2022-04-06 22:21:30 -0300

    coisas ae

--- core/views.py
+++ core/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
+from .models import Produto
 
 def index (request):
+    produtos = Produto.objects.all()
+    #print(dir(request.user))
+    #print(f"User:{request.user}")
+    #if str(request.user) == 'AnonymousUser':
+    #    teste = 'Usuario não logado'
+    #else:
+    #    teste = 'Usuario logado'
     context = {
         'curso' : 'programacao web com django',
         'outro' : 'so pra testar',
+        'produtos' : produtos
+    #    'logado' : teste
     }
     return render(request, 'index.html', context)
 
 def contato(request):
         return render(request, 'contato.html')
+
+def produto(request, pk):
+    print(f'PK: {pk}')
+    prod = Produto.objects.get(id=pk)
+
+    context = {
+        'produto' : prod
+    }
+    return render(request, 'produto.html', context)
